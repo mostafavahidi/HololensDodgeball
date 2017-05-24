c69a062640d249c2ef0b3cb33aad4005eaef8cc6
jasonc1221 <jasonc1221@gmail.com>
2017-05-23 17:32:31 -0700

    Dodgeball Unity

---
+++ Dodgeball Bouncing/Assets/Scripts/Gravity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(15f, -20f, 30f);
+        }
+	}
+}
